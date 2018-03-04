0d8a7288dfff553766877c3c06a166a5ea6fa404
classhopper <classhopp3r@gmail.com>
2018-03-04 17:16:05 +0200

    races_spec 02 OK

---
+++ app/models/event.rb
@@ -0,0 +1,34 @@
+class Event
+  include Mongoid::Document
+
+  field :o, as: :order, type: Integer
+  field :n, as: :name, type: String
+  field :d, as: :distance, type: Float
+  field :u, as: :units, type: String
+
+  def meters
+    case self.u
+      when "meters" then
+        self.d
+      when "kilometers" then
+        self.d * 1000
+      when "yards" then
+        self.d * 0.9144
+      when "miles" then
+        self.d * 1609.34
+    end if self.d
+  end
+
+  def miles
+    case self.u
+      when "meters" then
+        self.d * 0.000621371
+      when "kilometers" then
+        self.d * 0.621371
+      when "yards" then
+        self.d * 0.000568182
+      when "miles" then
+        self.d
+    end if self.d
+  end
+end
