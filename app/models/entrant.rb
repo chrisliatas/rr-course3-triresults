f5e02d71449e8b2ff6f73571def9fe645a81c728
classhopper <classhopp3r@gmail.com>
2018-03-04 17:40:47 +0200

    results_spec 01 OK

---
+++ app/models/entrant.rb
@@ -0,0 +1,11 @@
+class Entrant
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  store_in collection: 'results'
+  field :bib, type: Integer
+  field :secs, type: Float
+  field :o, as: :overall, type: Placing
+  field :gender,  type: Placing
+  field :group, type: Placing
+end
