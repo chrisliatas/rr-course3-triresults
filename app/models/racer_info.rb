6cb620a96a68aa98bd85bbd51ae79531e920d398
classhopper <classhopp3r@gmail.com>
2018-03-04 17:03:31 +0200

    racers_spec 04 OK

--- app/models/racer_info.rb
+++ app/models/racer_info.rb
@@ -7,7 +7,11 @@ class RacerInfo
   field :yr, as: :birth_year, type: Integer
   field :res, as: :residence, type: Address
   field :racer_id, as: :_id
-  field :_id, default: ->{racer_id}
+  field :_id, default: -> { racer_id }
 
   embedded_in :parent, polymorphic: true
+
+  validates_presence_of :first_name, :last_name, :gender, :birth_year
+  validates :gender, inclusion: { in: %w[M F] }
+  validates :birth_year, numericality: { less_than: Date.current.year }
 end
