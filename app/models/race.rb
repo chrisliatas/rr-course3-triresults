0ef6960e59a25d175770bdf0f50aaef665bf3038
classhopper <classhopp3r@gmail.com>
2018-03-04 17:08:09 +0200

    races_spec 01 OK

---
+++ app/models/race.rb
@@ -0,0 +1,8 @@
+class Race
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :n, as: :name, type: String
+  field :date, type: Date
+  field :loc, as: :location, type: Address
+end
