0a0ed4a63568736da9bb1592eb446be32ee98e81
classhopper <classhopp3r@gmail.com>
2018-03-05 17:49:05 +0200

    flatten_attributes_spec 03 OK

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -13,6 +13,15 @@ class Entrant
   embeds_one :race, class_name: 'RaceRef', autobuild: true
   embeds_one :racer, class_name: 'RacerInfo', as: :parent, autobuild: true
 
+  delegate :first_name, :first_name=, to: :racer
+  delegate :last_name, :last_name=, to: :racer
+  delegate :gender, :gender=, to: :racer, prefix: "racer"
+  delegate :birth_year, :birth_year=, to: :racer
+  delegate :city, :city=, to: :racer
+  delegate :state, :state=, to: :racer
+  delegate :name, :name=, to: :race, prefix: "race"
+  delegate :date, :date=, to: :race, prefix: "race"
+
   def update_total(result)
     self.secs = results.reduce(0) do |total, result|
       total + result.secs.to_i
@@ -22,4 +31,21 @@ class Entrant
   def the_race
     race.race
   end
+
+  def overall_place
+    overall.place if overall
+  end
+
+  def gender_place
+    gender.place if gender
+  end
+
+  def group_name
+    group.name if group
+  end
+
+  def group_place
+    group.place if group
+  end
+
 end
--- app/models/racer_info.rb
+++ app/models/racer_info.rb
@@ -25,4 +25,33 @@ class RacerInfo
       self.residence=object
     end
   end
+
+  DEFAULT_EVENTS = { "swim" => { :order=>0, :name=>"swim", :distance=>1.0, :units=>"miles" },
+                     "t1" =>   { :order=>1, :name=>"t1" },
+                     "bike" => { :order=>2, :name=>"bike", :distance=>25.0, :units=>"miles" },
+                     "t2" =>   { :order=>3, :name=>"t2" },
+                     "run" =>  { :order=>4, :name=>"run", :distance=>10.0, :units=>"kilometers" } }
+
+  DEFAULT_EVENTS.keys.each do |name|
+    define_method("#{name}") do
+      event=events.select {|event| name==event.name}.first
+      event||=events.build(DEFAULT_EVENTS["#{name}"])
+    end
+    ["order","distance","units"].each do |prop|
+      if DEFAULT_EVENTS["#{name}"][prop.to_sym]
+        define_method("#{name}_#{prop}") do
+          event=self.send("#{name}").send("#{prop}")
+        end
+        define_method("#{name}_#{prop}=") do |value|
+          event=self.send("#{name}").send("#{prop}=", value)
+        end
+      end
+    end
+  end
+
+  def self.default
+    Race.new do |race|
+      DEFAULT_EVENTS.keys.each {|leg|race.send("#{leg}")}
+    end
+  end
 end
