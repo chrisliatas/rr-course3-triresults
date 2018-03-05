6453894ca3f8e254a58c65cd70f6922f198d935d
classhopper <classhopp3r@gmail.com>
2018-03-05 18:28:31 +0200

    service_facade_spec 05 OK

--- app/models/race.rb
+++ app/models/race.rb
@@ -82,4 +82,9 @@ class Race
     end
     entrant
   end
+
+  def self.upcoming_available_to racer
+    upcoming_race_ids = racer.races.upcoming.pluck(:race).map {|r| r[:_id]}
+    upcoming.not_in(:id => upcoming_race_ids)
+  end
 end
