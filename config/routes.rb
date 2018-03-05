2914c67358a87bf870cd62a134ab68e047de3242
classhopper <classhopp3r@gmail.com>
2018-03-05 19:18:50 +0200

    web_registration_spec 01 OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :racers
+  resources :racers do
+    post "entries" => "racers#create_entry"
+  end
   resources :races
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
