29ebbbf1ba1623930adf0cf3efdfcc615372d6ee
classhopper <classhopp3r@gmail.com>
2018-03-05 18:40:02 +0200

    web_resources_spec 02 OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :racers
+  resources :races
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
