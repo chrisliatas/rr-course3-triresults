d9d59e1ba71b8824a063f577492d98806bd517a0
classhopper <classhopp3r@gmail.com>
2018-03-06 22:37:23 +0200

    uri_spec 03 OK

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -1,4 +1,18 @@
 module Api
   class RacesController < ApplicationController
+    def index
+      if !request.accept || request.accept == "*/*"
+        render plain: "/api/races"
+      else
+        #real implementation ...
+      end
+    end
+
+    def show
+      if !request.accept || request.accept == "*/*"
+        render plain: "/api/races/#{params[:id]}"
+      else
+      end
+    end
   end
 end
--- app/controllers/api/results_controller.rb
+++ app/controllers/api/results_controller.rb
@@ -1,4 +1,17 @@
 module Api
   class ResultsController < ApplicationController
+    def index
+      if !request.accept || request.accept == "*/*"
+        render plain: "/api/races/#{params[:race_id]}/results"
+      else
+      end
+    end
+
+    def show
+      if !request.accept || request.accept == "*/*"
+        render plain: "/api/races/#{params[:race_id]}/results/#{params[:id]}"
+      else
+      end
+    end
   end
 end
