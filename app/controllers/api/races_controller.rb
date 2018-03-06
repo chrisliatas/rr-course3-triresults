7178a086376bbada9bb3c9c415ce9c0771d48a7f
classhopper <classhopp3r@gmail.com>
2018-03-06 22:49:52 +0200

    uri_spec 04 OK

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -12,6 +12,23 @@ module Api
       if !request.accept || request.accept == "*/*"
         render plain: "/api/races/#{params[:id]}"
       else
+        # @race = Race.find(params[:id])
+        # render :race
+      end
+    end
+
+    def create
+      if !request.accept || request.accept == "*/*"
+        render plain: :nothing, status: :ok
+        # render plain: params[:race][:name]
+      # else
+      #   @race = Race.new(race_params)
+      #
+      #   if @race.save
+      #     render plain: race_params[:name], status: :created
+      #   else
+      #     render json: @race.errors
+      #   end
       end
     end
   end
