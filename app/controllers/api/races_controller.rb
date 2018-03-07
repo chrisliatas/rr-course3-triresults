7fa66b8d91c50a47f933a97ff6b43b0d00bf4fc8
classhopper <classhopp3r@gmail.com>
2018-03-07 16:05:58 +0200

    methods_spec 01 OK

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -20,16 +20,35 @@ module Api
     def create
       if !request.accept || request.accept == "*/*"
         # render plain: :nothing, status: :ok
-        render plain: params[:race][:name]
-      # else
-      #   @race = Race.new(race_params)
-      #
-      #   if @race.save
-      #     render plain: race_params[:name], status: :created
-      #   else
-      #     render json: @race.errors
-      #   end
+        render plain: "#{params[:race][:name]}", status: :ok
+      else
+        @race = Race.new(race_params)
+        if @race.save
+          render plain: race_params[:name], status: :created
+        else
+          render json: @race.errors
+        end
       end
     end
+
+    def update
+      @race = Race.find(params[:id])
+      if @race.update(race_params)
+        render json: @race
+      else
+        render json: @race.errors
+      end
+    end
+
+    def destroy
+      Race.find(params[:id]).destroy
+      render nothing: true, status: :no_content
+    end
+
+    private
+    def race_params
+      params.require(:race).permit(:name, :date)
+    end
+
   end
 end
