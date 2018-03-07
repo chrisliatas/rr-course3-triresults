6d6c4825f6113389f7771da73b0f73d649e0c834
classhopper <classhopp3r@gmail.com>
2018-03-07 22:08:35 +0200

    Finished?

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -1,5 +1,20 @@
 module Api
   class RacesController < ApplicationController
+
+    rescue_from Mongoid::Errors::DocumentNotFound do |exception|
+      @msg = "woops: cannot find race[#{params[:id]}]"
+
+      if !request.accept || request.accept == "*/*"
+        render plain: @msg, status: :not_found
+      else
+        render action: :error, status: :not_found
+      end
+    end
+
+    rescue_from ActionView::MissingTemplate do |exception|
+      render plain: "woops: unsupported content-type[#{request.accept}]", :status => 415
+    end
+
     def index
       if !request.accept || request.accept == "*/*"
         render plain: "/api/races, offset=[#{params[:offset]}], limit=[#{params[:limit]}]"
@@ -10,10 +25,10 @@ module Api
 
     def show
       if !request.accept || request.accept == "*/*"
-        render plain: "/api/races/#{params[:id]}"
+        render plain: api_race_path(params[:id])
       else
-        # @race = Race.find(params[:id])
-        # render :race
+        @race = Race.find(params[:id])
+        render "race", content_type: "#{request.accept}"
       end
     end
 
--- app/controllers/api/results_controller.rb
+++ app/controllers/api/results_controller.rb
@@ -4,6 +4,11 @@ module Api
       if !request.accept || request.accept == "*/*"
         render plain: "/api/races/#{params[:race_id]}/results"
       else
+        # implementation for the masses
+        @race = Race.find(params[:race_id])
+        @entrants = @race.entrants
+        if stale? last_modified: @entrants.max(:updated_at)
+          render :index
       end
     end
 
@@ -11,7 +16,42 @@ module Api
       if !request.accept || request.accept == "*/*"
         render plain: "/api/races/#{params[:race_id]}/results/#{params[:id]}"
       else
+        @result = Race.find(params[:race_id]).entrants.where(:id=>params[:id]).first
+        render :show
       end
     end
+
+    def update
+      #initialize the results entry with the race event
+      #update the time for the result
+    entrant = Race.find(params[:race_id]).entrants.where(:id=>params[:id]).first
+      result = params[:result]
+      if result
+        if result[:swim]
+          entrant.swim = entrant.race.race.swim
+          entrant.swim_secs = result[:swim].to_f
+        end
+        if result[:t1]
+          entrant.t1 = entrant.race.race.t1
+          entrant.t1_secs = result[:t1].to_f
+        end
+        if result[:bike]
+          entrant.bike = entrant.race.race.bike
+          entrant.bike_secs = result[:bike].to_f
+        end
+        if result[:t2]
+          entrant.t2 = entrant.race.race.t2
+          entrant.t2_secs = result[:t2].to_f
+        end
+        if result[:run]
+          entrant.run = entrant.race.race.run
+          entrant.run_secs = result[:run].to_f
+        end
+      end
+      entrant.save
+      render plain: :nothing, status: :ok
+    end
+
+  end
   end
 end
